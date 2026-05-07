26d30f92c38adf16717da3227b9a9de450f74795
Pyae Phyo Thaw <ppthaw1998@gmail.com>
2026-05-07 10:57:16 +0630

    fix error PIP

--- backend/src/test/java/com/epms/backend/service/PipServiceProbationRuleTest.java
+++ backend/src/test/java/com/epms/backend/service/PipServiceProbationRuleTest.java
@@ -27,6 +27,7 @@ import com.epms.backend.repository.FollowUpMeetingRepository;
 import com.epms.backend.repository.PipObjectiveRepository;
 import com.epms.backend.repository.PipProgressUpdateRepository;
 import com.epms.backend.repository.PipRepository;
+import com.epms.backend.repository.SignatureRepository;
 import com.epms.backend.repository.TrainingRecordRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,6 +44,8 @@ class PipServiceProbationRuleTest {
     @Mock
     private TrainingRecordRepository trainingRepository;
     @Mock
+    private SignatureRepository signatureRepository;
+    @Mock
     private EmployeeRepository employeeRepository;
     @Mock
     private NotificationService notificationService;
@@ -58,6 +61,7 @@ class PipServiceProbationRuleTest {
                 meetingRepository,
                 trainingRepository,
                 employeeRepository,
+                signatureRepository,
                 notificationService);
     }
 
